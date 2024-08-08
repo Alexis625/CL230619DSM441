ccdeeea51bcc357bf6934d1d44f3e53092de6799
Alexis625 <bluciano658@gmail.com>
2024-08-08 16:56:53 -0600

    Ejercicio01

---
+++ Desafio_01/Ejercicio01/app/src/main/java/com/example/ejercicio01/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.ejercicio01
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var input1: EditText
+    private lateinit var input2: EditText
+    private lateinit var input3: EditText
+    private lateinit var input4: EditText
+    private lateinit var input5: EditText
+    private lateinit var Calcular: Button
+    private lateinit var textViewResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
